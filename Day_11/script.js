0972ac1b73bd0ccfe6f6b5f78673325243731b0f
Rohan <ping.techyrohan@gmail.com>
2025-05-07 19:39:39 +0545

    Additional code added

---
+++ Day_11/script.js
@@ -0,0 +1,31 @@
+// ------------ IF ELSE IF LADDER -------------------
+
+let number = 20;
+
+if (number <= 10 && number >= 0) {
+  console.log('Number is less than or equal to 10 or greater than equal to 0');
+} else if (number >= 10 && number <= 20) {
+  console.log('Number is less than or equal to 20 or greater than equal to 10');
+} else {
+  console.log('None of the condition matched');
+}
+
+// INPUT GPA FROM USER, AND FIND ITS EQUIVALENT GRADE : EG (3.7 IS A+)
+
+// INPUT AGE FROM USER AND PERFORM THESE OPERATION BASED ON AGE RANGE:
+
+// 0 - 10 print "You are infant"
+// 11 - 20 print "You are young"
+// 21 - 40 print "You are adult"
+// 41 - 80 print "You are old"
+// 81 - 100 print "You are too old"
+// else "You were never borned or already in heaven ..."
+
+// -------------------------------------------------------------------
+// SCALAR (ALL DIFFERENT)
+// EQUILATERAL (ALL SIDE EQUAL)
+// ISOCELES (2 SIDE EQUAL)
+
+// -----------------------------------------------
+
+// INPUT THREE NUMBERS AND FIND GREATEST AMONG THEM
---
+++ Day_12/script.js
@@ -0,0 +1,49 @@
+// -------------------LOOPS------------------------------
+
+// WAP TO PRINT YOUR NAME 50 TIMES (DON'T BOTHER TO USE LOOPS)
+
+// FOR LOOP
+// WHILE LOOP
+// DO WHILE LOOP
+//  OTHER DIFFERS FROM TASTE OF LANGUAGES
+
+// SYNTAX: for(initialize_count; condition; increment/decrement) { }
+
+// for (let count = 1; count <= 100; count++) {
+//   console.log('Your name');
+// }
+
+// WAP TO PRINT NUMBER FROM 1 TO 100
+// WAP TO PRINT NUMBER FROM 100 TO 1
+// WAP TO PRINT EVEN NUMBER FROM 1 TO 100
+
+// for (let counter = 2; counter <= 100; counter = counter + 2) {
+//   console.log(counter);
+// }
+
+// WAP TO PRINT ODD NUMBER FROM A TO B
+// TAKE A AND B FROM USER
+// IF USER GIVES A=2 YOU HAVE TO START PRINTING FROM 3
+
+let A = Number(prompt('Enter first number')); // 11
+let B = Number(prompt('Enter second number')); // 10
+
+for (let count = A; count <= B; count++) {
+  if (count % 2 == 1) {
+    console.log(count);
+  }
+}
+
+// BOYS: PRINT TABLE OF 5
+// GIRLS: PRINT TABLE OF 2
+
+// output should be in this format
+// 5 X 1 = 5
+// 5 X 2 = 10
+// 5 X 3 = 15
+// 5 X 4= 20
+// 5 X 5 = 25
+
+for (let count = 1; count <= 10; count++) {
+  console.log(`5 X ${count} = ${5 * count}`);
+}
