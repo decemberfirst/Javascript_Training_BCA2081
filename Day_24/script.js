7c5b0807e872039cad522efce999e336e49f3857
Rohan <ping.techyrohan@gmail.com>
2025-06-04 13:17:52 +0545

    Code added

--- Day_24/script.js
+++ Day_24/script.js
@@ -1,36 +1,39 @@
-// ------------- SHALLOW COPY AND DEEP COPY ------------------
+// // ------------- SHALLOW COPY AND DEEP COPY ------------------
 
-let a = 10;
-let b = a;
-b = 30;
-console.log(a); // 10
-console.log(b); // 30
+// let a = 10;
+// let b = a;
+// b = 30;
+// console.log(a); // 10
+// console.log(b); // 30
 
-let person = {
-  personName: 'Ram',
-};
+// let person = {
+//   personName: 'Ram',
+// };
 
-let anotherPerson = person;
-console.log(anotherPerson);
+// let anotherPerson = person;
+// console.log(anotherPerson);
 
-person.personName = 'CHANGED_NAME';
+// person.personName = 'CHANGED_NAME';
 
-console.log(person); // {personName: "Ram"}
-console.log(anotherPerson); // {personName: "CHANGED_NAME"}
+// console.log(person); // {personName: "Ram"}
+// console.log(anotherPerson); // {personName: "CHANGED_NAME"}
 
-// -------------------- PASS BY VALUE VS PASS BY REFERENCE ---------
+// // -------------------- PASS BY VALUE VS PASS BY REFERENCE ---------
 
-// 1) Pass by value
+// PRIMITIVE TYPE -> number, boolean, NaN, Symbol and many more
+// NON-PRIMITIVE -> object, array
 
-let first = 10;
-let second = first; // second = 10
+// // 1) Pass by value
 
-first = 20;
+// let first = 10;
+// let second = first; // second = 10
 
-console.log(a);
-console.log(b);
+// first = 20;
 
-// ------------ FOR SPECIAL TYPES -------------
+// console.log(a);
+// console.log(b);
+
+// // ------------ FOR SPECIAL TYPES -------------
 
 let myFirstObject = {
   personName: 'Ram',
@@ -40,22 +43,119 @@ let myCopyOfFirstObject = myFirstObject;
 
 myFirstObject.personName = 'Shyam';
 
-let dogObject = {
-  animalName: 'Dog',
-  color: 'Shade of black',
+// let dogObject = {
+//   animalName: 'Dog',
+//   color: 'Shade of black',
+// };
+
+// let anotherDog = dogObject; // pass by reference , pass by address
+
+// dogObject.color = 'Red';
+
+// console.log(anotherDog);
+// console.log(dogObject);
+
+// let firstName = 'Ram';
+// let secondName = firstName; // Pass by value
+
+// secondName = 'SHYAM';
+
+// console.log(firstName); // Ram
+// console.log(secondName); // SHYAM
+
+let myNestedObject = {
+  address: {
+    permanent: 'Butwal',
+    temporary: 'Pokhara',
+  },
 };
 
-let anotherDog = dogObject; // pass by reference , pass by address
+let myNestedObjectCopy = structuredClone(myNestedObject); // now it works as pass by value
+myNestedObjectCopy.address.permanent = 'Kathmandu';
 
-dogObject.color = 'Red';
+// let myNestedObjectCopy = myNestedObject;
 
-console.log(anotherDog);
-console.log(dogObject);
+// myNestedObject.address.permanent = 'KATHMANDU';
 
-let firstName = 'Ram';
-let secondName = firstName; // Pass by value
+console.log(myNestedObjectCopy);
+console.log(myNestedObject);
 
-secondName = 'SHYAM';
+// TODO:
+// WAP ON YOUR OWN TO DEMONSTRATE -> PASS BY VALUE AND PASS BY REFERENCE
+// FINALLY USE structuredClone to one of your program
 
-console.log(firstName); // Ram
-console.log(secondName); // SHYAM
+// ------------------------------------------------------------------------------------------------------------
+
+// TODO:
+// CREATE ONE PERSON OBJECT WITH NAME -> FIRST NAME, LAST NAME, ADDRESS -> TEMPORARY AND PERMANENT ADDRESS,
+// COPY PERSON OBJECT TO THREE DIFFERENT VARIABLES
+// CHANGE NAME OF ALL VARIABLES
+// MAKE SURE THAT CHANGE IN ONE OBJECT PROPERTY DOESNOT REFLECT TO ANOTHER ONE
+
+// ------------------------------- FUNCTION -------------------------------------
+// F = 2 + 2
+// F(X) =  2X + 2
+// LET ABC(X) =  X^2 + 4
+// F(2) -> 8
+
+// LET F(X) = CURRENT_TIME + X
+
+// LET F(2) = 2 + 2 = 4
+
+// F(2) =  6 + 2 = 8
+// DETERMINISTIC ,
+
+// -----------------------------------------------------------
+// shadowing
+// function F(x) {
+//   console.log(2 * PI + 2); // RESULT
+//   PI = 1; // FUNCTION SIDE EFFECT
+// }
+
+// F(10);
+
+function myEquation(x) {
+  console.log(x + 2);
+}
+
+myEquation(2); // myEquation(2) / f(2)
+myEquation(3);
+myEquation(0);
+myEquation(-10);
+
+// f(x,y) = x + y
+// f(2,2) = 2 + 2 = 4
+// f(-10,2) = -8
+// f(0,0) = 0 + 0 = 0
+
+function add(x, y) {
+  console.log(x + y);
+}
+
+add(2, 2);
+add(4, 4);
+add(10, 30);
+
+// create function for subraction
+
+let personObject = {
+  personName: 'hari',
+};
+
+let anotherPersonName = structuredClone(personObject);
+
+anotherPersonName.personName = 'shyam';
+
+console.log(personObject);
+console.log(anotherPersonName);
+
+function Addition(X, Y) {
+  // function definition
+  console.log(Y);
+  console.log(2 + 2 + X + Y);
+}
+
+Addition(10); // function call, invoke
+
+// ---------- create a function to print name-------
+// use parameters or take input from parameter
